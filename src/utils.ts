79e7179709c514a5dedc8f03a5877aefffc25d59
Stéphane TRÉBEL (WeScale) <stephane.trebel@wescale.fr>
2026-03-16 16:33:15 +0100

    chore: apply formatting

--- src/utils.ts
+++ src/utils.ts
@@ -18,11 +18,16 @@ const config: {
 }
 
 export function getConfig(): Required<typeof config> {
-  config.grafanaUrl = config.grafanaUrl ? removeTrailingSlash(config.grafanaUrl) : removeTrailingSlash(requiredEnv('GRAFANA_URL'))
-  config.keycloakProtocol = config.keycloakProtocol ?? requiredEnv('KEYCLOAK_PROTOCOL')
-  config.keycloakDomain = config.keycloakDomain ?? requiredEnv('KEYCLOAK_DOMAIN')
+  config.grafanaUrl = config.grafanaUrl
+    ? removeTrailingSlash(config.grafanaUrl)
+    : removeTrailingSlash(requiredEnv('GRAFANA_URL'))
+  config.keycloakProtocol
+    = config.keycloakProtocol ?? requiredEnv('KEYCLOAK_PROTOCOL')
+  config.keycloakDomain
+    = config.keycloakDomain ?? requiredEnv('KEYCLOAK_DOMAIN')
   config.keycloakRealm = config.keycloakRealm ?? requiredEnv('KEYCLOAK_REALM')
-  config.keycloakToken = config.keycloakToken ?? requiredEnv('KEYCLOAK_ADMIN_PASSWORD')
+  config.keycloakToken
+    = config.keycloakToken ?? requiredEnv('KEYCLOAK_ADMIN_PASSWORD')
   config.keycloakUser = config.keycloakUser ?? requiredEnv('KEYCLOAK_ADMIN')
   // @ts-ignore
   return config
